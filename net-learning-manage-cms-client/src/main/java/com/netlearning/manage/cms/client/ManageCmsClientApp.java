b71ce993aed3a911ea5b64b5d80f9d9b8d1219e3
GitBoyXuChang <574616156@qq.com>
2019-10-14 10:02:46 +0800

    注册中心搭建

--- net-learning-manage-cms-client/src/main/java/com/netlearning/manage/cms/client/ManageCmsClientApp.java
+++ net-learning-manage-cms-client/src/main/java/com/netlearning/manage/cms/client/ManageCmsClientApp.java
@@ -3,15 +3,15 @@ package com.netlearning.manage.cms.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
 @SpringBootApplication
 @EntityScan("com.netlearning.framework.domain.cms")//扫描实体类
 @ComponentScan(basePackages={"com.netlearning.api"})//扫描api接口
 @ComponentScan(basePackages={"com.netlearning.manage.cms.client"})//扫描本项目下的所有类
 @ComponentScan(basePackages={"com.netlearning.framework"})
+@EnableEurekaClient
 public class ManageCmsClientApp {
 
 	public static void main(String[] args) {
--- net-learning-manage-cms/src/main/java/com/netlearning/manage/cms/ManageCmsApp.java
+++ net-learning-manage-cms/src/main/java/com/netlearning/manage/cms/ManageCmsApp.java
@@ -3,18 +3,19 @@ package com.netlearning.manage.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+
 @SpringBootApplication
 @EntityScan("com.netlearning.framework.domain.cms")//扫描实体类
 @ComponentScan(basePackages={"com.netlearning.api"})//扫描api接口
 @ComponentScan(basePackages={"com.netlearning.manage.cms"})//扫描本项目下的所有类
 @ComponentScan(basePackages={"com.netlearning.framework"})
+@EnableEurekaClient
 public class ManageCmsApp {
 
 	public static void main(String[] args) {
